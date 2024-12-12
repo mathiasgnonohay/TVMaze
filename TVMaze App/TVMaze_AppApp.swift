17257523bd156a6abf761be55c4c94d52a7056d2
Mathias Nonohay <mathiasnonohay@icloud.com>
2024-12-12 11:38:36 -0300

    Initial Commit

---
+++ TVMaze App/TVMaze_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVMaze_AppApp.swift
+//  TVMaze App
+//
+//  Created by Mathias Nonohay on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TVMaze_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
